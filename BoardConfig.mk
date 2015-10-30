481c77e2e1f332c165bc5917e43cb8f3a55d2b20
Vince Scott <c0d3x42@gmail.com>
2015-10-30 16:41:08 +0000

    i

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_CUSTOM_BOOTIMG_MK := device/vodafone/p839v55/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.mode=normal loglevel=4
 
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 0x800
+BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x02000000
