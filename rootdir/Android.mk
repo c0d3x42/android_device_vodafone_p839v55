f1cb24d5b648d79d4b1df5e215a6aef901f967e8
Vince Scott <c0d3x42@gmail.com>
2015-12-18 11:40:00 +0000

    fixups

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -108,7 +108,7 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.target.rc
+LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
